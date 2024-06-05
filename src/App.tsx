68294304d38f14ed9ab7fb85f9c68dd51dd1385a
ledzz <ledzz1994@gmail.com>
2024-06-05 14:06:52 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import {Fullscreen, Image, Text} from "@react-three/uikit";
+import {Canvas} from "@react-three/fiber";
+
+function App() {
+
+  return (
+      <Canvas>
+          <Fullscreen flexDirection="row" padding={100} gap={100}>
+              <Image src="https://picsum.photos/200/300" alignItems={"center"} justifyContent={"center"}>
+                  <Text fontSize={24} backgroundColor={0} color={0xffff}>
+                      Hello world
+                  </Text>
+              </Image>
+          </Fullscreen>
+      </Canvas>
+  )
+}
+
+export default App
