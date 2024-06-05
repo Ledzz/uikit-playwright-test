008113efabc007265c961beb2a9f478b934fb788
ledzz <ledzz1994@gmail.com>
2024-06-05 14:46:53 +0300

    wip`

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
       <Canvas>
           <Fullscreen flexDirection="row" padding={100} gap={100}>
-              <Image src="https://picsum.photos/200/300" alignItems={"center"} justifyContent={"center"}>
+              <Image src="https://picsum.photos/id/13/200/300" alignItems={"center"} justifyContent={"center"}>
                   <Text fontSize={24} backgroundColor={0} color={0xffff}>
                       Hello world
                   </Text>
